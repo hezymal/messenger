8a2e2acefffac3398076d14d6e901505d7a6c0ed
hezymal <hezymal@gmail.com>
2022-10-14 22:30:19 +0300

    (fix) remove 'web-api' service

--- app/web-api/src/setup/setupMongo.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-import { DI } from "../di";
-
-export const setupMongo = async (di: DI) => {
-    const client = await MongoClient.connect("mongodb://root:root@localhost:27017/");
-    di.setMongo(client);
-};
